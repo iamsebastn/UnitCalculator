74a545ce550cacb911f78ee0e8e1a73b85c001d7
Sebastian Wolf <sebastian.wolf298@gmail.com>
2023-01-20 10:28:26 +0100

    reasembled the code

--- data.js
+++ data.js
@@ -1,1 +1,14 @@
-
+export const conversions = [
+    length = {
+        title: "Length (Meter/Feet)",
+        ratio: 3.281 // 1m = 3.281 feet
+    },
+    volume = {
+        title: "Volume (Liter/Gallons)",
+        ratio: 0.264 // 1 liter = 2.204 gallons
+    },
+    mass = {
+        title: "Mass (Kilograms/Pounds)",
+        ratio: 2.204 // 1 kg = 2.204 pounds
+    }
+]
--- script.js
+++ script.js
@@ -3,22 +3,37 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+import { conversions } from "./data"
 
-
-const convertBtn = document.getElementById("convert-btn")
-
-convertBtn.addEventListener("click", () => {
+document.getElementById("convert-btn").addEventListener("click", () => {
     const input = document.getElementById("input").value
     if(input === "") {
-        convertBtn.disabled = true
+        alert("Please insert a value to convert")
     } else {
-        document.getElementById("length-mf").textContent = `${(input / 3.821).toFixed(3)}m`
-        document.getElementById("length-fm").textContent = `${(input * 3.821).toFixed(3)}ft`
-    
-        document.getElementById("length-gl").textContent = `${(input / 0.264).toFixed(3)}gl`
-        document.getElementById("length-lg").textContent = `${(input * 0.264).toFixed(3)}l`
-    
-        document.getElementById("length-pk").textContent = `${(input / 2.204).toFixed(3)}pnd`
-        document.getElementById("length-kp").textContent = `${(input * 2.204).toFixed(3)}kg`
+        document.getElementById("length").textContent = `
+        ${(input / 3.821).toFixed(3)} meter(s) | ${(input * 3.821).toFixed(3)} feet
+        `
+        document.getElementById("volume").textContent = `
+        ${(input / 0.264).toFixed(3)} liter(s) | ${(input * 0.264).toFixed(3)} galon(s)
+        `
+        document.getElementById("mass").textContent = `
+        ${(input * 2.204).toFixed(3)} kilogram | ${(input / 2.204).toFixed(3)} pound(s)
+        `
     }
 })
+
+function render() {
+    const conversionWr = document.getElementById("conversion-parent")
+    conversionWr.innerHTML = conversions.forEach((item) => {
+    `
+        <div class="conversion-item">
+            <h3 class="txt-h3">${item.title}</h3>
+            <p class="txt-eye" id="length">- | -</p>
+        </div>
+    `   
+    })
+    console.log(conversions[0].title + "item")
+}
+
+render(conversions)
+console.log(conversions)
